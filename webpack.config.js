6ec0cd00064c6a2db6ecca4d2515eb4e6ee1fa48
tvanantwerp <tom@tomvanantwerp.com>
2017-03-01 17:28:32 -0500

    Lint the webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,14 @@
 const webpack = require('webpack');
+
 const nodeEnv = process.env.NODE_ENV || 'production';
 
 module.exports = {
   devtool: 'soure-map',
   entry: {
-    filename: './src/js/app.js'
+    filename: './src/js/app.js',
   },
   output: {
-    filename: './dist/js/stateMap.js'
+    filename: './dist/js/stateMap.js',
   },
   module: {
     loaders: [
@@ -16,21 +17,21 @@ module.exports = {
         exclude: /node_modules/,
         loader: 'babel-loader',
         query: {
-          presets: ['es2015-native-modules']
-        }
-      }
-    ]
+          presets: ['es2015-native-modules'],
+        },
+      },
+    ],
   },
   plugins: [
     // uglify
     new webpack.optimize.UglifyJsPlugin({
       compress: { warnings: false },
-      output: { comments:false },
-      sourceMap: true
+      output: { comments: false },
+      sourceMap: true,
     }),
     // environment
     new webpack.DefinePlugin({
-      'process.env': { NODE_ENV: JSON.stringify(nodeEnv) }
-    })
-  ]
+      'process.env': { NODE_ENV: JSON.stringify(nodeEnv) },
+    }),
+  ],
 };
